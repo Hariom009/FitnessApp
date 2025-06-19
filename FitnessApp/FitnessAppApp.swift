4ad7a0b9925620d6998b3541ccce265841247095
Hariom009 <hariomconnects03@gmail.com>
2025-06-19 11:03:37 +0530

    Initial Commit

---
+++ FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Hari's Mac on 19.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
